e85d411f99095183f4efe0743e6fbb44adc7ccd1
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-18 22:32:46 -0700

    for loops and list comprehension methods

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,32 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    # badges = []
+    # for name in names:
+    #     badges.append(f"Hello, my name is {name}.")
+    
+    badges = [f"Hello, my name is {name}." for name in names] 
+    return badges
 
 def assign_rooms(names):
-    return None
+    # rooms = []
+    # for index in range(len(names)):
+    #     rooms.append(f"Hello, {names[index]}! You'll be assigned to room {index + 1}!")
+
+    rooms = [f"Hello, {names[index]}! You'll be assigned to room {index + 1}!" 
+             for index in range(len(names))]
+    return rooms
+
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+
+    # for badge in badges:
+    #     print(badge)
+
+    # for room in rooms:
+    #     print(room)
+    badges = [print(badge) for badge in badges]
+    rooms = [print(room) for room in rooms]
